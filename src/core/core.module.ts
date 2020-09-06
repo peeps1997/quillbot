8c7610d23879364ca622089ac4ccdffab539e2ce
Abhishek Shekhawat <abhishekshekhawat99@gmail.com>
2020-09-07 04:13:54 +0530

    added base route

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -4,12 +4,13 @@ import { APP_PIPE } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { CoreController } from './core.controller';
 
 @Module({
   imports: [CommentsModule, ConfigModule.forRoot({ isGlobal: true }), ServeStaticModule.forRoot({
     rootPath: join(__dirname, '..', 'client'),
   }),],
-  controllers: [],
+  controllers: [CoreController],
   providers: [{ provide: APP_PIPE, useClass: ValidationPipe }],
 })
 export class CoreModule { }
